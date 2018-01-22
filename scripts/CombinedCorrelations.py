2496a39f5a116202e5ef166b8b88111144642dc9
vgoel30 <varun.goel@stonybrook.edu>
2018-01-22 16:29:54 +0530

    Shifted to cleaner methods

--- scripts/CombinedCorrelations.py
+++ scripts/CombinedCorrelations.py
@@ -6,6 +6,13 @@ import seaborn as sns
 import math
 import numpy as np; np.random.seed(0)
 from gini import gini
+import os
+
+L_countries = {}
+nations_path = '../data/Nations.txt'
+with open(nations_path) as nations_file:
+	for nation in nations_file:
+		L_countries[nation.replace('\n','')] = 0
 
 def csv_writer(params, values_lists, path):
     with open(path, "w") as csv_file:
@@ -21,13 +28,7 @@ def minutes_parser(minutes_string):
 		return 0
 	return int(minutes_string.replace('\'','').replace('.',''))
 
-L_countries = {}
-nations_path = '../data/Nations.txt'
-with open(nations_path) as nations_file:
-	for nation in nations_file:
-		L_countries[nation.replace('\n','')] = 0
-
-def get_team_percentage(data, year, team):
+def get_league_percentage(country, data, year, team):
 	age = 0
 	local = 0
 	foreign = 0
@@ -63,6 +64,53 @@ def get_team_percentage(data, year, team):
 	total = local_minutes + foreign_minutes
 	return [(foreign_minutes/total)*100, average_age, diversity]
 
+def get_combined_correlations():
+	years = [year for year in range(1995, 2017)]
+	params = ['Won', 'Lost', 'Draw', 'GF', 'GA',  'PPM', 'Foreign playing time %', 'Average age', 'diversity']
+	countries = ['England', 'France', 'Germany', 'Italy', 'Spain']
+	rows = []
+
+	for country in countries:
+		data_file = '../data/Leistungsdaten/' + str(country) + '/2016.json'
+
+		for year in years:
+			path = '../data/Standings/' + country + '/' + str(year) + '.csv'
+
+			with open(path, "r") as file_obj:
+				reader = csv.DictReader(file_obj, delimiter=',')
+
+				with open(data_file) as datafile:
+					data = json.load(datafile)
+					for line in reader:
+
+						#new row data for each team
+						row = []
+						team_name = line['Name']
+						# pprint(team_name + ':  ' + str(get_team_percentage(data, '2001', team_name)))
+						# pprint(line)
+						row.append(int(line['Won']))
+						row.append(int(line['Lost']))
+						row.append(int(line['Draw']))
+						row.append(int(line['GF']))
+						row.append(int(line['GA']))
+						#row.append(int(line['Points']))
+						row.append(float(line['PPM']))
+						vals = get_league_percentage(country, data, str(year), team_name)
+
+						row.append(vals[0])
+						row.append(vals[1])
+						row.append(vals[2])
+
+						rows.append(row)
+			print(year)
+	pprint(rows)
+
+	csv_writer(params, rows, 'lol.csv')
+
+	df = pd.read_csv('lol.csv')
+	os.remove('lol.csv')   
+	return df 
+
 def csv_dict_reader(file_obj):
     """
     Read a CSV file using csv.DictReader
@@ -80,49 +128,7 @@ def csv_reader(file_obj):
     for row in reader:
         print(" ".join(row))
 
-years = [year for year in range(1995, 2017)]
-params = ['Won', 'Lost', 'Draw', 'GF', 'GA',  'PPM', 'Foreign playing time %', 'Average age', 'diversity']
-countries = ['England', 'France', 'Germany', 'Italy', 'Spain']
-rows = []
-
-for country in countries:
-	data_file = '../data/Leistungsdaten/' + str(country) + '/2016.json'
-
-	for year in years:
-		path = '../data/Standings/' + country + '/' + str(year) + '.csv'
-
-		with open(path, "r") as file_obj:
-			reader = csv.DictReader(file_obj, delimiter=',')
-
-			with open(data_file) as datafile:
-				data = json.load(datafile)
-				for line in reader:
-
-					#new row data for each team
-					row = []
-					team_name = line['Name']
-					# pprint(team_name + ':  ' + str(get_team_percentage(data, '2001', team_name)))
-					# pprint(line)
-					row.append(int(line['Won']))
-					row.append(int(line['Lost']))
-					row.append(int(line['Draw']))
-					row.append(int(line['GF']))
-					row.append(int(line['GA']))
-					#row.append(int(line['Points']))
-					row.append(float(line['PPM']))
-					vals = get_team_percentage(data, str(year), team_name)
-
-					row.append(vals[0])
-					row.append(vals[1])
-					row.append(vals[2])
-
-					rows.append(row)
-		print(year)
-pprint(rows)
-
-csv_writer(params, rows, 'lol.csv')
-
-df = pd.read_csv('lol.csv')    
+df = get_combined_correlations()
 corr = df.corr()
 sns.set(font_scale=0.8)
 sns.heatmap(corr, cmap="YlGnBu", annot=True, annot_kws={"size": 13})
